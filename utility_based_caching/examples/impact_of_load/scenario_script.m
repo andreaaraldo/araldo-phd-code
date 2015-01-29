ef6669db26635e570b4e2a44ccf046ef6f9e6ce5
Andrea Araldo <andrea.araldo@gmail.com>
2015-01-29 15:21:43 +0100

    AA

--- utility_based_caching/examples/impact_of_load/scenario_script.m
+++ utility_based_caching/examples/impact_of_load/scenario_script.m
@@ -21,7 +21,7 @@ server = 2;
 load_ = 1.00;
 total_requests = 1921 * load_;
 arcs = "{<2, 1, 490000>};"; % In Kbps
-max_storage_at_single_as = (catalog_size / 100) * \
+max_storage_at_single_as = (catalog_size / 100) * ...
 						(cache_space_per_quality(2) + cache_space_per_quality(3) )/2  ; % IN MB
 max_cache_storage = max_storage_at_single_as; % IN Mpbs
 seeds = 1:3;
@@ -39,10 +39,11 @@ for seed = seeds
 			ASes_with_users, server, total_requests,
 			arcs, max_storage_at_single_as, max_cache_storage, seed, filename_prefix);
 	
-	command = sprintf("oplrun %s/oplproj/oplproj.mod %s", path_base, dat_filename);
-	if ( system(command ) != 0)
-		error(sprintf("ERROR in the execution of command %s", command) );
-	endif
+	
+#	command = sprintf("oplrun %s/oplproj/oplproj.mod %s", path_base, dat_filename);
+#	if ( system(command ) != 0)
+#		error(sprintf("ERROR in the execution of command %s", command) );
+#	endif
 
 	file_to_read = sprintf("%s/oplproj/objective.csv", path_base);
 	value = dlmread(file_to_read);
