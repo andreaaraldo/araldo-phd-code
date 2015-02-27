dff1271fab3cb21d579b61a6808ad5ec3a4f126e
andrea_at_ptech_laptop <andrea.araldo@gmail.com>
2015-02-27 23:54:42 +0100

    AA

--- cost_aware/graph/prova_graph_tool.py
+++ cost_aware/graph/prova_graph_tool.py
@@ -5,3 +5,4 @@ vp, ep = graph_tool.centrality.betweenness(g)
 # vp stores the betwennes centrality of each vertex
 # For other centrality measures, see http://graph-tool.skewed.de/static/doc/centrality.html#graph_tool.centrality.betweennesshttp://graph-tool.skewed.de/static/doc/centrality.html#graph_tool.centrality.betweenness
 print(vp)
+type(vp)
