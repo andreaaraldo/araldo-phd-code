b0eabdccfbfcf887dd4703da6d696e9082d6a1cf
Andrea Araldo <andrea.araldo@gmail.com>
2015-02-27 19:39:13 +0100

    AA

--- cost_aware/graph/prova_graph_tool.py
+++ cost_aware/graph/prova_graph_tool.py
@@ -1,1 +1,7 @@
 from graph_tool.all import *
+
+g2 = load_graph("graphs_pictorical_from_dario/geant.dot")
+vp, ep = graph_tool.centrality.betweenness(g)
+# vp stores the betwennes centrality of each vertex
+# For other centrality measures, see http://graph-tool.skewed.de/static/doc/centrality.html#graph_tool.centrality.betweennesshttp://graph-tool.skewed.de/static/doc/centrality.html#graph_tool.centrality.betweenness
+print(vp)
