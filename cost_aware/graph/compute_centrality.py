be025234f65335a84c9b20833af0d7bcff5a9fc0
andrea_at_ptech_laptop <andrea.araldo@gmail.com>
2015-02-28 01:35:23 +0100

    AA

--- cost_aware/graph/compute_centrality.py
+++ cost_aware/graph/compute_centrality.py
@@ -5,6 +5,6 @@ vp, ep = graph_tool.centrality.betweenness(g, norm=False)
 # vp stores the betwennes centrality of each vertex
 # For other centrality measures, see http://graph-tool.skewed.de/static/doc/centrality.html#graph_tool.centrality.betweennesshttp://graph-tool.skewed.de/static/doc/centrality.html#graph_tool.centrality.betweenness
 
-for v in g.vertices()
+for v in g.vertices():
 	print(v.in_degree() )
 
