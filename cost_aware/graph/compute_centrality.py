2388aa5921d49a09486ae12d63c926bc2f245aa1
Andrea from loganberry <andrea.araldo@gmail.com>
2015-02-28 01:14:00 +0100

    AA

--- cost_aware/graph/compute_centrality.py
+++ cost_aware/graph/compute_centrality.py
@@ -4,7 +4,7 @@ g = load_graph("graphs_pictorical_from_dario/geant.dot")
 vp, ep = graph_tool.centrality.betweenness(g)
 # vp stores the betwennes centrality of each vertex
 # For other centrality measures, see http://graph-tool.skewed.de/static/doc/centrality.html#graph_tool.centrality.betweennesshttp://graph-tool.skewed.de/static/doc/centrality.html#graph_tool.centrality.betweenness
-print(vp.get_array())
-print(ep)
+print(vp)
+#print(ep)
 
 
