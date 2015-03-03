b5aae8648c6a878eaae4f6affde92007c034d0b7
andrea_at_ptech_laptop <andrea.araldo@gmail.com>
2015-03-03 20:52:38 +0100

    AA

--- cost_aware/che/leondardi_c/execute.bash
+++ cost_aware/che/leondardi_c/execute.bash
@@ -1,15 +1,12 @@
+#!/bin/bash
 rm -f general_tree_extended.o
 gcc general_tree_extended.c -lm -o general_tree_extended.o
 
-PI=10
-SEED=1
-./general_tree_extended.o $PI $SEED > results/results-pi_$PI-seed_$SEED.log
 
-exit
-for PI in 1 2 5 10 ;
+for PI in 10000 ;
 do
-	for SEED in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 ;
-		do 
-			./general_tree_extended.o $PI $SEED > results/results-pi_$PI-seed_$SEED.log
-		done
+	for SEED in 1 2 3 ;
+	do 
+		./general_tree_extended.o $PI $SEED > results/results-pi_$PI-seed_$SEED.log
+	done
 done
