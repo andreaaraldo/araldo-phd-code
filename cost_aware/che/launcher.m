fc2a394f45250554ea31d0e9c301aab70838fa93
Andrea Araldo <andrea.araldo@gmail.com>
2015-03-03 11:27:56 +0100

    AA: working with che and cost

--- cost_aware/che/launcher.m
+++ cost_aware/che/launcher.m
@@ -8,7 +8,10 @@ TC = 31.688717870764198291331729873124;
 
 for lambda_tot = [1, 100]
 	a=5;
-	f_name = sprintf('TC-ctlg_%g-csize_%g-alpha_%g-lambda_tot_%g', N, B, alpha, 5)
+	f_name = sprintf('TC-ctlg_%g-csize_%g-alpha_%g-lambda_tot_%g.dat', N, B, alpha, lambda_tot);
+
+	
+
 	[P_zipf, lambda_obj] = prepare_zipf(N,B,lambda_tot,alpha);
 
 	[pHitChe, pHitCheAvg_LCE] = che_LCE(P_zipf, lambda_obj, TC);
