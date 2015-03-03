d73e902590f5a59b92719fefa77c0c5053bb808b
Andrea Araldo <andrea.araldo@gmail.com>
2015-03-03 17:29:17 +0100

    AA

--- cost_aware/che/leondardi_c/execute.bash
+++ cost_aware/che/leondardi_c/execute.bash
@@ -1,5 +1,8 @@
 rm -f general_tree_extended.o
 gcc general_tree_extended.c -lm -o general_tree_extended.o
 
-rm -f general_tree_original.o
-gcc general_tree_original.c -lm -o general_tree_original.o
+for PI in 10 ;
+do
+	./general_tree_extended.o $PI 
+	# > results_$PI.dat
+done
