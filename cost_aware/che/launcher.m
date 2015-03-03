e13338362c65d245990166f7327d4db6c757248b
Andrea Araldo <andrea.araldo@gmail.com>
2015-03-03 11:09:48 +0100

    AA: working with che and cost

--- cost_aware/che/launcher.m
+++ cost_aware/che/launcher.m
@@ -4,7 +4,14 @@ B = 100;
 lambda_tot = 4;
 alpha = 1;
 TC = 31.688717870764198291331729873124;
+
+q = 0.01; % probability to accept an incoming object
+
 [P_zipf, lambda_obj] = prepare_zipf(N,B,lambda_tot,alpha);
-[pHitChe, pHitCheAvg] = che_LCE(P_zipf, lambda_obj, TC);
-pHitCheAvg
+[pHitChe, pHitCheAvg_LCE] = che_LCE(P_zipf, lambda_obj, TC);
+pHitCheAvg_LCE
+
+[pHitChe, pHitCheAvg_unif] = che_LCE(P_zipf, lambda_obj, TC, q);
+pHitCheAvg_unif
+
 exit
