5c865b67c3a0785eeb0fc15979b68a18e38e43c4
andrea from cranberry <andrea.araldo@gmail.com>
2015-03-20 12:12:28 +0100

    AA

--- ccnsim_support/scripts-result_processing/parse_results.m
+++ ccnsim_support/scripts-result_processing/parse_results.m
@@ -10,7 +10,7 @@ out_folder="/tmp/long_sim_time/"; % where to put results
 optimization_result_folder="~/shared_with_servers/icn14_runs/greedy_algo-NESSUNA";
 resultdir="~/software/ccnsim/results/sim_vs_che";
 
-id_rep_list=[1:40]; # list of seeds
+id_rep_list=[1:20]; # list of seeds
 
 priceratio_list={"10","1.111","1.25","1.429","1.667","2","2.5","3.333","5"};
 priceratio_list={"10"};
