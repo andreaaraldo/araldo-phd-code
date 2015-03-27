1513f0ffe5f9685538f24090824a211a19e38644
andrea from cranberry <andrea.araldo@gmail.com>
2015-03-27 17:00:35 +0100

    AA

--- ccnsim_support/scripts-result_processing/parse_results.m
+++ ccnsim_support/scripts-result_processing/parse_results.m
@@ -10,11 +10,11 @@ out_folder="/tmp/"; % where to put results
 optimization_result_folder="~/shared_with_servers/icn14_runs/greedy_algo-NESSUNA";
 resultdir="~/software/ccnsim/results/sim_vs_che";
 
-id_rep_list=[1:11,15:20]; # list of seeds
+id_rep_list=[1:5]; # list of seeds
 
 priceratio_list={"10","1.111","1.25","1.429","1.667","2","2.5","3.333","5"};
 priceratio_list={"10"};
-priceratio_list={"1","10","100"};
+priceratio_list={"1"};
 
 
 % The decision policies that I want to plot
@@ -22,7 +22,7 @@ decision_list={"lce","fix0.01","costaware0.01","tailandrank","costprobtailperf",
 decision_list={"lce","fix0.1", "fix0.05", "fix0.01", "fix0.005", "fix0.001", "fix0.0001", "fix0.00001"};
 decision_list={"lce","fix0.01","costaware0.01","costprobprodplain0.01", "costprobcoincorr0.5", "costprobcoinplain0.5", "costprobcoincorr0.1", "costprobcoinplain0.1","costprobcoincorr0.01", "costprobcoinplain0.01"};
 decision_list={"lce","fix0.01","tailandrank","costaware0.01","costprobtailcons"};
-decision_list={"costaware0.01"};
+decision_list={"fix0.01","costaware0.01"};
 
 xi_list = {"0.01","0.025","0.05","0.075","0.25","0.50","0.75","1","1.25","1.50","1.75","2","3","5","8"};
 xi_list = {"0.01","0.25","0.50","0.75","1","1.25","1.50","1.75","2"};
@@ -33,7 +33,7 @@ xi_list = {"1"};
 weights_list={"0.5_0.25_0.25","0.333_0.333_0.334","0.25_0.25_0.5"};
 weights_list={"0.333_0.333_0.334","0.5_0.25_0.25","0.25_0.25_0.5"};
 weights_list={"0.333_0.333_0.334","0_0.25_0.75", "0_0.5_0.5", "0_0.75_0.25", "0.25_0_0.75", "0.25_0.25_0.5", "0.25_0.5_0.25", "0.25_0.75_0", "0.5_0.25_0.25", "0.5_0.5_0", "0.75_0_0.25", "0.75_0.25_0","0.5_0.5_0"};
-weights_list={"0.333_0.333_0.334"};
+weights_list={"0.5_0.5_0"};
 
 alpha_list = {"0.8","1","1.2"};
 alpha_list = {"1"};
@@ -43,7 +43,7 @@ alpha_list = {"1"};
 window_list = {"60"};
 variance_list = {"0.05"};
 
-lambda_list = {"1000"};
+lambda_list = {"100"};
 
 q_list={"0"};
 
@@ -62,7 +62,7 @@ forwarding_list={"nrr"};
 replacement_="lru";
 
 simtime_list = {"1800","18000","180000","1800000","9000000"};
-simtime_list = {"1800000"};
+simtime_list = {"18000"};
 
 csize_list = {"1e3"};
 csize_to_write_list = csize_list;
