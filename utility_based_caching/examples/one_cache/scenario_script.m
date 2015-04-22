f54e1c88330e64a2ba94d31d7b3ebe36b1a34566
Andrea Arakdi <andrea.araldo@gmail.com>
2015-04-22 12:07:16 +0200

    AA

--- utility_based_caching/examples/one_cache/scenario_script.m
+++ utility_based_caching/examples/one_cache/scenario_script.m
@@ -52,7 +52,6 @@ data.alphas = [1];
 
 data.loadds = [0.1, 0.5, 0.8, 1, 1.2, 1.5, 2]; 	% Multiple of link capacity we would use to transmit 
 				% all the requested objects at low quality
-data.loadds = [1];
 
 data.strategys = {"RepresentationAware", "NoCache", "AlwaysLowQuality", "AlwaysHighQuality", "AllQualityLevels", "DedicatedCache", "ProportionalDedicatedCache"};
 
