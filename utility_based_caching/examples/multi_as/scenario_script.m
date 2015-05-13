ab98e2170e573c4eb70bee96a097671e981c3cd3
araldo_tipi53 <andrea.araldo@gmail.com>
2015-05-13 12:15:06 +0200

    AA

--- utility_based_caching/examples/multi_as/scenario_script.m
+++ utility_based_caching/examples/multi_as/scenario_script.m
@@ -18,7 +18,7 @@ run_ = true;
 experiment_name = "multi_as";
 
 data.fixed_datas = [];
-fixed_data.parallel_processes = 1;
+fixed_data.parallel_processes = 7;
 fixed_data.path_base = path_base;
 fixed_data.rate_per_quality = [0, 300, 700, 1500, 2500, 3500]; % In Kpbs
 fixed_data.cache_space_at_low_quality = 11.25;% In MB
@@ -32,7 +32,7 @@ fixed_data.name = "cubic";
 
 fixed_data.utilities = [0**(1/4)/5**(1/4), 1**(1/4)/5**(1/4), 2**(1/4)/5**(1/4), 3**(1/4)/5**(1/4), 4**(1/4)/5**(1/4), 5**(1/4)/5**(1/4)];
 fixed_data.name = "power4";
-%data.fixed_datas = [data.fixed_datas, fixed_data];
+data.fixed_datas = [data.fixed_datas, fixed_data];
 
 
 fixed_data.utilities = [0, 1/5, 2/5, 3/5, 4/5, 5/5 ];
@@ -61,15 +61,15 @@ topology.arcs = lines{2};
 topology.ases_with_storage = 1:size_;
 topology.name = sprintf("size_%d-edgenodes_%d-capacity_%g-toposeed_%d-ubiquitous", ...
 		size_, edge_nodes, topology.link_capacity, topology_seed);
-%data.topologys = [data.topologys, topology];
+data.topologys = [data.topologys, topology];
 
 topology.ases_with_storage = topology.ASes_with_users;
 topology.name = sprintf("size_%d-edgenodes_%d-capacity_%g-toposeed_%d-edge", ...
 		size_, edge_nodes, topology.link_capacity, topology_seed);
 data.topologys = [data.topologys, topology];
 
-data.solutiongaps = [0.0001]; # default 0.0001 (0.01%)
-data.timelimits = [10 100 1000]; # default 1e75
+data.solutiongaps = [0.01]; # default 0.0001 (0.01%)
+data.timelimits = [1e75]; # default 1e75
 data.seeds = [1];
 data.catalog_sizes = [1000];
 data.cache_to_ctlg_ratios = [edge_nodes/100];	% fraction of catalog we could store in the cache if all 
