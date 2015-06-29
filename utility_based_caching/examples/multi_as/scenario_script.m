b80653bc78ce2c52b407a3f485937c7da2969870
andrea from logan <andrea.araldo@gmail.com>
2015-06-29 14:42:54 +0200

    AA

--- utility_based_caching/examples/multi_as/scenario_script.m
+++ utility_based_caching/examples/multi_as/scenario_script.m
@@ -32,17 +32,17 @@ fixed_data.name = "cubic";
 
 fixed_data.utilities = [0**(1/4)/5**(1/4), 1**(1/4)/5**(1/4), 2**(1/4)/5**(1/4), 3**(1/4)/5**(1/4), 4**(1/4)/5**(1/4), 5**(1/4)/5**(1/4)];
 fixed_data.name = "power4";
-%data.fixed_datas = [data.fixed_datas, fixed_data];
+data.fixed_datas = [data.fixed_datas, fixed_data];
 
 
 fixed_data.utilities = [0, 1/5, 2/5, 3/5, 4/5, 5/5 ];
 fixed_data.name = "linear";
-data.fixed_datas = [data.fixed_datas, fixed_data];
+%data.fixed_datas = [data.fixed_datas, fixed_data];
 
 %{TOPOLOGY
 data.topologys = [];
 size_ = 10;
-edge_nodess = [size_/2];
+edge_nodess = [size_];
 topology.link_capacity = 490000;  % In Kbps
 topology_seed = 2;
 
@@ -75,7 +75,7 @@ end % for edge_nodes
 data.cache_allocations = {"constrained"}; # constrained or free
 data.solutiongaps = [0.01]; # default 0.0001 (that means 0.01%)
 data.timelimits = [14400]; # default 1e75
-data.seeds = [1 2];
+data.seeds = [1];
 data.catalog_sizes = [1000];
 data.cache_to_ctlg_ratios = [edge_nodes/100];	% fraction of catalog we could store in the cache if all 
 						% the objects were at maximum quality
