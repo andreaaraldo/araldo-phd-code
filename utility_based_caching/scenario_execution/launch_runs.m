b50f4fc596dcba5375ad5982beb2594dbc9ee9c2
Andrea Araldo <andrea.araldo@gmail.com>
2015-07-08 14:07:40 +0200

    AA

--- utility_based_caching/scenario_execution/launch_runs.m
+++ utility_based_caching/scenario_execution/launch_runs.m
@@ -4,7 +4,7 @@ function launch_runs(experiment_name, data);
 	run_scenarios(run_list);
 	seeds = unique([run_list.seed] );
 
-	error("I do not want to parse now");
+	%error("I do not want to parse now");
 	##############################
 	##### PARSE OUTPUT ###########
 	##############################
@@ -53,35 +53,36 @@ function launch_runs(experiment_name, data);
 						endif
 						value = dlmread(file_to_read,' ',1,0);
 						utility(:,:,idx_seed)  = value;
-						file_to_read = sprintf("%s/quality_served_per_rank.csv",  seed_folder);
-						if (!length(quality_served_header))
-							f = fopen(file_to_read, "r");
-							quality_served_header = fgetl(f);
-							fclose(f);
-						endif
-						value = dlmread(file_to_read,' ',1, 0);
-						quality_served(:,:,idx_seed) = value;
+
+#						file_to_read = sprintf("%s/quality_served_per_rank.csv",  seed_folder);
+#						if (!length(quality_served_header))
+#							f = fopen(file_to_read, "r");
+#							quality_served_header = fgetl(f);
+#							fclose(f);
+#						endif
+#						value = dlmread(file_to_read,' ',1, 0);
+#						quality_served(:,:,idx_seed) = value;
 
 
-						file_to_read = sprintf("%s/quality_cached_per_rank.csv",  seed_folder);
-						if (!length(quality_cached_per_rank_header))
-							f = fopen(file_to_read, "r");
-							quality_cached_per_rank_header = fgetl(f);
-							fclose(f);
-						endif
-						value = dlmread(file_to_read,' ',1, 0);
-						quality_cached_per_rank(:,:,idx_seed) = value;
+#						file_to_read = sprintf("%s/quality_cached_per_rank.csv",  seed_folder);
+#						if (!length(quality_cached_per_rank_header))
+#							f = fopen(file_to_read, "r");
+#							quality_cached_per_rank_header = fgetl(f);
+#							fclose(f);
+#						endif
+#						value = dlmread(file_to_read,' ',1, 0);
+#						quality_cached_per_rank(:,:,idx_seed) = value;
 
 
 
-						file_to_read = sprintf("%s/unsatisfied_ratio.csv",  seed_folder);
-						if (!length(unsatisfied_ratio_header) )
-							f = fopen(file_to_read, "r");
-							unsatisfied_ratio_header = fgetl(f);
-							fclose(f);
-						endif
-						value = dlmread(file_to_read,' ',1,0);
-						unsatisfied_ratio(:,:,idx_seed)  = value;
+#						file_to_read = sprintf("%s/unsatisfied_ratio.csv",  seed_folder);
+#						if (!length(unsatisfied_ratio_header) )
+#							f = fopen(file_to_read, "r");
+#							unsatisfied_ratio_header = fgetl(f);
+#							fclose(f);
+#						endif
+#						value = dlmread(file_to_read,' ',1,0);
+#						unsatisfied_ratio(:,:,idx_seed)  = value;
 				endfor % seed
 
 				%{ CHECK
@@ -104,38 +105,38 @@ function launch_runs(experiment_name, data);
 								"append","on", "delimiter"," ");
 			
 
-						file_to_write = sprintf("%s/quality_served.dat",singledata.parent_folder);
-						quality_served_mean = nanmean(quality_served, dim_to_reduce);
-						quality_served_confidence = confidence_interval(quality_served, 
-								dim_to_reduce, ignore_NaN=true);
-						f = fopen(file_to_write, "w+");
-						fprintf(f, "#%s %s\n",quality_served_header, quality_served_header );
-						fclose(f);
-						dlmwrite(file_to_write,[quality_served_mean, quality_served_confidence],
-								"append","on", "delimiter"," ");
+#						file_to_write = sprintf("%s/quality_served.dat",singledata.parent_folder);
+#						quality_served_mean = nanmean(quality_served, dim_to_reduce);
+#						quality_served_confidence = confidence_interval(quality_served, 
+#								dim_to_reduce, ignore_NaN=true);
+#						f = fopen(file_to_write, "w+");
+#						fprintf(f, "#%s %s\n",quality_served_header, quality_served_header );
+#						fclose(f);
+#						dlmwrite(file_to_write,[quality_served_mean, quality_served_confidence],
+#								"append","on", "delimiter"," ");
 
 
 
-						file_to_write = sprintf("%s/quality_cached_per_rank.dat",singledata.parent_folder);
-						quality_cached_per_rank_mean = nanmean(quality_cached_per_rank, dim_to_reduce);
-						quality_cached_per_rank_confidence = confidence_interval(quality_cached_per_rank, 
-								dim_to_reduce, ignore_NaN=true);
-						f = fopen(file_to_write, "w+");
-						fprintf(f, "#%s %s\n",quality_cached_per_rank_header, quality_cached_per_rank_header );
-						fclose(f);
-						dlmwrite(file_to_write,[quality_cached_per_rank_mean, quality_cached_per_rank_confidence],
-								"append","on", "delimiter"," ");
+#						file_to_write = sprintf("%s/quality_cached_per_rank.dat",singledata.parent_folder);
+#						quality_cached_per_rank_mean = nanmean(quality_cached_per_rank, dim_to_reduce);
+#						quality_cached_per_rank_confidence = confidence_interval(quality_cached_per_rank, 
+#								dim_to_reduce, ignore_NaN=true);
+#						f = fopen(file_to_write, "w+");
+#						fprintf(f, "#%s %s\n",quality_cached_per_rank_header, quality_cached_per_rank_header );
+#						fclose(f);
+#						dlmwrite(file_to_write,[quality_cached_per_rank_mean, quality_cached_per_rank_confidence],
+#								"append","on", "delimiter"," ");
 
 
-						file_to_write = sprintf("%s/unsatisfied_ratio.dat", singledata.parent_folder);
-						unsatisfied_ratio_mean = nanmean(unsatisfied_ratio, dim_to_reduce);
-						unsatisfied_ratio_confidence = confidence_interval(
-												unsatisfied_ratio, dim_to_reduce, ignore_NaN=true);
-						f = fopen(file_to_write, "w+");
-						fprintf(f, "#%s %s\n",unsatisfied_ratio_header, unsatisfied_ratio_header );
-						fclose(f);
-						dlmwrite(file_to_write,[unsatisfied_ratio_mean, unsatisfied_ratio_confidence], 
-								"append","on", "delimiter"," ");
+#						file_to_write = sprintf("%s/unsatisfied_ratio.dat", singledata.parent_folder);
+#						unsatisfied_ratio_mean = nanmean(unsatisfied_ratio, dim_to_reduce);
+#						unsatisfied_ratio_confidence = confidence_interval(
+#												unsatisfied_ratio, dim_to_reduce, ignore_NaN=true);
+#						f = fopen(file_to_write, "w+");
+#						fprintf(f, "#%s %s\n",unsatisfied_ratio_header, unsatisfied_ratio_header );
+#						fclose(f);
+#						dlmwrite(file_to_write,[unsatisfied_ratio_mean, unsatisfied_ratio_confidence], 
+#								"append","on", "delimiter"," ");
 			printf("Experiment %s parsed\n", singledata.parent_folder);
 		%else
 			% the experiment has already been parsed: I do nothing
