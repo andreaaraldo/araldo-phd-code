9d4a0ee422eb336a12deec59f7bec7aa14cb3651
andrea from cranberry <andrea.araldo@gmail.com>
2015-07-10 14:17:47 +0200

    AA

--- utility_based_caching/examples/multi_as/scenario_script.m
+++ utility_based_caching/examples/multi_as/scenario_script.m
@@ -75,11 +75,7 @@ end % for edge_nodes
 data.cache_allocations = {"constrained"}; # constrained or free
 data.solutiongaps = [0.01]; # default 0.0001 (that means 0.01%)
 data.timelimits = [28800]; # default 1e75
-<<<<<<< HEAD
-data.seeds = [1 2 3 5];
-=======
 data.seeds = [1];
->>>>>>> 293748f01ccd75d43f3a330f7019bba1eb6e921b
 data.catalog_sizes = [1000];
 data.cache_to_ctlg_ratios = [5/100];	% fraction of catalog we could store in the overall cache space
 											% if all the objects were at maximum quality
