14e9c60e9f04c0fe54bb101c98709fca4d167707
andrea from cranberry <andrea.araldo@gmail.com>
2015-07-13 12:51:48 +0200

    AA

--- utility_based_caching/examples/multi_as/scenario_script.m
+++ utility_based_caching/examples/multi_as/scenario_script.m
@@ -26,7 +26,7 @@ fixed_data.cache_space_at_low_quality = 11.25;% In MB
 
 fixed_data.utilities = [0, 1**(1/4)/5**(1/4), 2**(1/4)/5**(1/4), 3**(1/4)/5**(1/4), 4**(1/4)/5**(1/4), 5**(1/4)/5**(1/4)];
 fixed_data.name = "power4";
-data.fixed_datas = [data.fixed_datas, fixed_data];
+%data.fixed_datas = [data.fixed_datas, fixed_data];
 
 
 fixed_data.utilities = [0, 1/5, 2/5, 3/5, 4/5, 5/5 ];
@@ -75,9 +75,9 @@ end % for edge_nodes
 data.cache_allocations = {"constrained"}; # constrained or free
 data.solutiongaps = [0.01]; # default 0.0001 (that means 0.01%)
 data.timelimits = [28800]; # default 1e75
-data.seeds = [5];
+data.seeds = 1;
 data.catalog_sizes = [1000];
-data.cache_to_ctlg_ratios = [5/100];	% fraction of catalog we could store in the overall cache space
+data.cache_to_ctlg_ratios = [1/100, 2/100];	% fraction of catalog we could store in the overall cache space
 											% if all the objects were at maximum quality
 data.alphas = [1];
 data.customtypes = {"float"}; % float or int
