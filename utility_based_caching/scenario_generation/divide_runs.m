1c918e873486aff2959bf1d9e9016292b672e1be
Andrea Araldo <andrea.araldo@gmail.com>
2015-07-22 10:35:39 +0200

    AA

--- utility_based_caching/scenario_generation/divide_runs.m
+++ utility_based_caching/scenario_generation/divide_runs.m
@@ -42,7 +42,8 @@ function run_list = divide_runs(experiment_name, data)
 			end%if
 		%} CHECK
 
-		topodescription
+		disp ('topodescription');
+		cat(topodescription);
 		error('ciao');
 
 		lines = strsplit(topodescription, del="\n");
