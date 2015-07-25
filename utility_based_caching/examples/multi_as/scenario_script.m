0ae8c237001e8c28a1abc513abf0655de9d614b3
andrea from cranberry <andrea.araldo@gmail.com>
2015-07-25 19:18:46 +0200

    AA

--- utility_based_caching/examples/multi_as/scenario_script.m
+++ utility_based_caching/examples/multi_as/scenario_script.m
@@ -31,26 +31,22 @@ data.fixed_datas = [data.fixed_datas, fixed_data];
 
 fixed_data.utilities = [0, 1/5, 2/5, 3/5, 4/5, 5/5 ];
 fixed_data.name = "linear";
-%data.fixed_datas = [data.fixed_datas, fixed_data];
+data.fixed_datas = [data.fixed_datas, fixed_data];
 
 data.seeds = [1];
 
 
 data.cache_allocations = {"constrained"}; # constrained or free
-<<<<<<< HEAD
-data.solutiongaps = [0.00009]; # default 0.0001 (that means 0.01%)
-=======
-data.solutiongaps = [0.01]; # default 0.0001 (that means 0.01%)
->>>>>>> 02e82b75eaec86f3b656095d6d8cad3c934b8ac1
+data.solutiongaps = [0.0100002]; # default 0.0001 (that means 0.01%)
 data.timelimits = [28800]; # default 1e75
-data.catalog_sizes = [500];
-data.cache_to_ctlg_ratios = [11/100];	% fraction of catalog we could store in the overall cache space
+data.catalog_sizes = [10000];
+data.cache_to_ctlg_ratios = [1/100];	% fraction of catalog we could store in the overall cache space
 											% if all the objects were at maximum quality
 data.alphas = [1];
 data.customtypes = {"float"}; % float or int	
 
 %{ TOPOLOGY
-	data.topofile="abilene";
+	data.topofile="";
 	strcmp(data.topofile,"")
 	if ( strcmp(data.topofile,"") )
 		% You did not specify a file. You want to generate it
@@ -72,11 +68,10 @@ data.customtypes = {"float"}; % float or int
 % It is expressed as a multiple of link capacity we would use to transmit 
 % all the requested objects at low quality
 data.loadds = [0.25 0.5 0.75 1 1.25 1.5 1.75 2];
-data.loadds = [1];
 
 % DedicatedCache excluded
 data.strategys = {"NoCache", "RepresentationAware", "AlwaysLowQuality", "AlwaysHighQuality", "AllQualityLevels", "PropDedCache"};
-
+data.strategys = {"NoCache", "RepresentationAware"};
 
 data.path_base= path_base;
 launch_runs(experiment_name, data);
