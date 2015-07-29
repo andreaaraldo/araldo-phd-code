f0cc5e753d073c5a87a4966b3e825231821fc3cd
Andrea Arakdi <andrea.araldo@gmail.com>
2015-07-29 10:26:53 +0200

    AA

--- utility_based_caching/examples/multi_as/scenario_script.m
+++ utility_based_caching/examples/multi_as/scenario_script.m
@@ -31,15 +31,15 @@ data.fixed_datas = [data.fixed_datas, fixed_data];
 
 fixed_data.utilities = [0, 1/5, 2/5, 3/5, 4/5, 5/5 ];
 fixed_data.name = "linear";
-data.fixed_datas = [data.fixed_datas, fixed_data];
+%data.fixed_datas = [data.fixed_datas, fixed_data];
 
-data.seeds = [1 2 4 5];
+data.seeds = [1 2 3 4 5];
 
 data.cache_allocations = {"constrained"}; # constrained or free
-data.solutiongaps = [0.0100002]; # default 0.0001 (that means 0.01%)
+data.solutiongaps = [0.01]; # default 0.0001 (that means 0.01%)
 data.timelimits = [57700]; # default 1e75
-data.catalog_sizes = [10000];
-data.cache_to_ctlg_ratios = [1/100];	% fraction of catalog we could store in the overall cache space
+data.catalog_sizes = [1000];
+data.cache_to_ctlg_ratios = [5/100];	% fraction of catalog we could store in the overall cache space
 											% if all the objects were at maximum quality
 data.alphas = [1];
 data.customtypes = {"float"}; % float or int	
@@ -47,8 +47,8 @@ data.customtypes = {"float"}; % float or int
 	data.topofile="";
 	if ( strcmp(data.topofile,"") )
 		% You did not specify a file. You want to generate it
-		data.topology_size = 2;
-		data.edge_nodess = [1]; % How many edge nodes
+		data.topology_size = 10;
+		data.edge_nodess = [5]; % How many edge nodes
 		data.link_capacity = 490000;  % In Kbps
 
 		data.arcss={""}; % You can specify explitly with opl model syntax
@@ -63,7 +63,7 @@ data.customtypes = {"float"}; % float or int
 			data.ases_with_storage = [2];
 		end%if 
 
-		data.server_positions = {"complement_to_edges"}; %"complement_to_edges", "edges" or "specific"
+		data.server_positions = {"edges"}; %"complement_to_edges", "edges" or "specific"
 		if ( strcmp(data.server_positions, "specific")  )
 			data.servers = [1];
 		end%if 
@@ -80,10 +80,10 @@ data.customtypes = {"float"}; % float or int
 % It is expressed as a multiple of link capacity we would use to transmit 
 % all the requested objects at low quality
 data.loadds = [0.25 0.5 0.75 1 1.25 1.5 1.75 2];
+data.loadds = [1]
 
 % DedicatedCache excluded
 data.strategys = {"NoCache", "RepresentationAware", "AlwaysLowQuality", "AlwaysHighQuality", "AllQualityLevels", "PropDedCache"};
-data.strategys = {"NoCache", "RepresentationAware"};
 
 data.path_base= path_base;
 launch_runs(experiment_name, data);
