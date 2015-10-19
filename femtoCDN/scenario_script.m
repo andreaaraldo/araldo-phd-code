419116b9f7de3952c9863a882791cd821e2f263e
Andrea Araldo <andrea.araldo@gmail.com>
2015-10-19 17:35:16 +0200

    starting to work on femtoCDN

--- femtoCDN/scenario_script.m
+++ femtoCDN/scenario_script.m
@@ -1,14 +1,16 @@
-%ciao
+%The algoritm implemented here is from 
+% Megory-Cohen, Igal and Ela, Givat - "Dynamic Cache Partitioning by Modified Steepest Descent" 
 global severe_debug = true;
 addpath("~/software/araldo-phd-code/utility_based_caching/scenario_generation");
 %pkg load statistics;
 %pkg load communications; % for randint
 
 
+rounding = false;
 
-alpha=[1.2; 1.0; 0.1];
-R = [1e8; 1e8; 1e8 ];
-catalog=[1e5; 1e5; 1e5];
+alpha=[1.2; 1.1];
+R = [1e6; 1e6 ];
+catalog=[1e3; 1e3];
 
 N = length(alpha); %num CPs
 K = 10; %cache slots
@@ -26,10 +28,10 @@ boost =  1;
 %}INITIALIZATION
 
 
-for i=1:100
+for i=1:10000
 	%{CHECK
 	if(severe_debug)
-		if ( sum( c < ones(N,1) ) != 0 )
+		if ( sum( round(c) < ones(N,1) ) != 0 )
 			c
 			error("Inconsistent c")
 		end	
@@ -46,17 +48,13 @@ for i=1:100
 	end%for
 	%}REQUEST GENERATION
 
-	m = sum(requests(:,c+1 : size(requests,2) ) , 2); % misses
+	m = sum(requests(:,round(c)+1 : size(requests,2) ) , 2); % misses
 
 	f = sum(requests, 2 ); % total requests
 	M = m*1.0./f; M(isnan(M) )=0; % Current miss ratio
 
-
-
-	% !!!!!!! BE CAREFUL: This is the modified formula
-	M_prime = (M .- LM)*1.0 ./ abs(c .- Lc )*(-1); % derivative of miss ratio
 	% This is the original formula
-	M_prime = (M .- LM)*1.0 ./ (c .- Lc ); % derivative of miss ratio
+	M_prime = (M .- LM)*1.0 ./ (round(c) .- round(Lc) ); % derivative of miss ratio
 
 	F = f / sum(f); % request frequency
 	r = (1.0/sqrt(N) ) * ones(N,1);
@@ -67,11 +65,24 @@ for i=1:100
 	Nc = (c .+ delta_c);
 
 	%{COPE WITH DISCRETE VALUES AND INCONSISTENCIES
-		Nc = round(Nc);
-		difference = sum(Nc) - K;
+		if (rounding) 
+			Nc = round(Nc);
+		end
+
+		while ( any( round(Nc) < ones(N,1) ) != 0 )
+			unlucky = unidrnd(N);
+			if (Nc(unlucky)>1 )
+				Nc(unlucky) = Nc(unlucky ) - 1;
+			end
+			lucky = min(find( round(Nc) < ones(N,1)) );
+			Nc(lucky) = Nc(lucky)+1;
+		end
+
+		difference = sum(round(Nc) ) - K;
 		while (difference > 0)
 			for d=1:difference
-				unlucky = unidrnd(N);
+				unlucky_candidates = find( round(Nc)>1 );
+				unlucky = unlucky_candidates(unidrnd( length(unlucky_candidates) ) );
 				if (Nc(unlucky)>1 )
 					Nc(unlucky) = Nc(unlucky ) - 1;
 					difference--;
@@ -84,14 +95,6 @@ for i=1:100
 			difference++;
 		end
 
-		while ( sum( Nc < ones(N,1) ) != 0 )
-			unlucky = unidrnd(N);
-			if (Nc(unlucky)>1 )
-				Nc(unlucky) = Nc(unlucky ) - 1;
-			end
-			lucky = unidrnd(N);
-			Nc(lucky) = Nc(lucky)+1;
-		end
 	%}COPE WITH DISCRETE VALUES AND INCONSISTENCIES
 
 	%{CHECK
@@ -118,8 +121,11 @@ for i=1:100
 
 
 
-	Lc (Nc != c) = c (Nc != c);
-	LM (Nc != c) = M (Nc != c);
+	updated_CPs = round(Nc) != round(c);
+	Lc(updated_CPs) = c (updated_CPs);
+	LM(updated_CPs) = M (updated_CPs);
 	c = Nc;
-end%for 
-c
+
+	disp(c')
+
+end%for
