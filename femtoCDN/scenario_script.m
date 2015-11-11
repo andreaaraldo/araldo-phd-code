7adf76c088bbf809b97df685bbb8d18eab512f6b
Andrea Araldo <andrea.araldo@gmail.com>
2015-11-11 11:29:12 +0100

    starting to work on femtoCDN

--- femtoCDN/scenario_script.m
+++ femtoCDN/scenario_script.m
@@ -1,7 +1,7 @@
 %script
 global severe_debug = 1;
 addpath("~/software/araldo-phd-code/utility_based_caching/scenario_generation");
-mdat_folder = "data/rawdata/prova";
+mdat_folder = "data/rawdata/with10";
 max_parallel = 7;
 
 settings.save_mdat_file = true;
