33893cdddadb05ccdc137b86b793521d0a60a53b
Andrea Araldo <andrea.araldo@gmail.com>
2015-11-19 14:03:53 +0100

    AA

--- femtoCDN/parse_results.m
+++ femtoCDN/parse_results.m
@@ -1,9 +1,10 @@
 function parse_results(in, settings)
-	load(settings.infile);
+	load(settings.outfile);
+	result_file = sprintf("%s.dat", settings.simname);
 	hist_value = [0];
 	for t=1:settings.epochs;
 		hist_value= [hist_value; compute_value(in, round(vc) ) ];
 	end
-	dlmwrite(settings.outfile,  [hist_cum_observed_req', round( hist_vc(1,:) )', hist_value ], " " );
+	dlmwrite(result_file,  [hist_cum_observed_req', round( hist_vc(1,:) )', hist_value ], " " );
 	printf("%s written", settings.outfile);
 end
