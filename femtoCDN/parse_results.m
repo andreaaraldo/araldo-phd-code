c91094f2f590d7ae71263f90b8dbab0f69323b42
Andrea Araldo <andrea.araldo@gmail.com>
2015-11-19 15:37:40 +0100

    AA

--- femtoCDN/parse_results.m
+++ femtoCDN/parse_results.m
@@ -6,5 +6,5 @@ function parse_results(in, settings)
 		hist_value= [hist_value; compute_value(in, round(vc) ) ];
 	end
 	dlmwrite(result_file,  [hist_cum_observed_req', round( hist_vc(1,:) )', hist_value ], " " );
-	printf("%s written", result_file);
+	printf("%s written\n", result_file);
 end
