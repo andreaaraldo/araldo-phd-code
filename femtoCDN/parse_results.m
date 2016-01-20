8055c4f445072e77735abea3da901e6ba4619a73
andrea from cranberry <andrea.araldo@gmail.com>
2016-01-20 16:05:42 +0100

    AA

--- femtoCDN/parse_results.m
+++ femtoCDN/parse_results.m
@@ -1,14 +1,17 @@
 function parse_results(in, settings)
-	ALL_HISTORY=1; FINAL_MSE=2; FINAL_OBSERVED_HIT=3;
-	output = FINAL_MSE;
+	ALL_HISTORY=1; FINAL_CV=2; FINAL_OBSERVED_HIT=3;
+	output = FINAL_CV;
 
 	load(settings.outfile);
 
 	[hist_allocation, hist_cum_tot_requests, hist_cum_hit] = compute_metrics(...
 		in, settings, hist_theta, hist_num_of_misses, hist_tot_requests);
 
-	hist_difference = ( hist_theta - repmat(theta_opt,1, size(hist_theta,2)) ) / in.K;
-	hist_MSE = meansq( hist_difference , 1 );
+	hist_difference = ( hist_theta - repmat(theta_opt,1, size(hist_theta,2)) );
+	hist_CV = sqrt( meansq( hist_difference , 1 ) ) ./ mean(hist_theta, 1) ;
+
+	hist_theta
+	mean(hist_theta, 1)
 
 	switch output
 		case ALL_HISTORY
@@ -21,8 +24,8 @@ function parse_results(in, settings)
 					round( hist_theta(1,:) )', hist_value, hist_MSE' ], " " );
 			printf("%s written\n", result_file);
 
-		case FINAL_MSE
-			v = hist_MSE(length(hist_MSE) );
+		case FINAL_CV
+			v = hist_CV( length(hist_CV ) );
 			printf("%s %g %g %g\n", settings.method, in.lambda, in.T, v);
 
 		case FINAL_OBSERVED_HIT
--- femtoCDN/scenario_script.m
+++ femtoCDN/scenario_script.m
@@ -4,19 +4,19 @@ addpath("~/software/araldo-phd-code/utility_based_caching/scenario_generation");
 mdat_folder = "data/rawdata/iteration_duration";
 max_parallel = 22;
 
-parse=false; % false if you want to run the experiment.
+parse=true; % false if you want to run the experiment.
 settings.save_mdat_file = true;
-overwrite = true;
+overwrite = false;
 methods_ = {"csda", "dspsa_orig", "opencache", "optimum"};
-methods_ = {"csda","dspsa_orig", "opencache"};
+methods_ = {"dspsa_orig"};
 normalizes = {"no", "max", "norm"};
 normalizes = {"no"};
 coefficientss = {"no", "simple", "every10","every100", "adaptive"};
 coefficientss = {"adaptive"};
 boosts = [1];
-lambdas = [1e2]; %req/s
-tot_times = [2]; %total time(hours)
-Ts = [10 100 1000]; % epoch duration (s)
+lambdas = [1]; %req/s
+tot_times = [0.5]; %total time(hours)
+Ts = [10]; % epoch duration (s)
 overall_ctlgs = [1e8];
 ctlg_epss = [0];
 alpha0s = [1];
@@ -24,7 +24,7 @@ alpha_epss = [0];
 req_epss = [-1]; % if -1, req_proportion must be explicitely set
 in.req_proportion=[0.28 0.28 0.28 0.04 0.02 0.02 0.02 0.02 0.02 0.02];
 ps = [10];
-Ks = [1e6]; %cache slots
+Ks = [1e3]; %cache slots
 seeds = [1];
 
 %{ CONSTANTS
