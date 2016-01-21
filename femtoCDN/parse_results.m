a8129528026a0aa0f463d9b17a7a709a48fa8cb4
Andrea Araldo <andrea.araldo@gmail.com>
2016-01-21 16:06:08 +0100

    AA

--- femtoCDN/parse_results.m
+++ femtoCDN/parse_results.m
@@ -2,6 +2,8 @@ function parse_results(in, settings)
 	ALL_HISTORY=1; FINAL_CV=2; FINAL_OBSERVED_HIT=3; FINAL_ERR = 4;
 	output = FINAL_ERR;
 
+	printf("\n\n\n\n I AM PRINTING %s\n", settings.method);
+
 	load(settings.outfile);
 	theta_opt = in.req_proportion' * in.K;
 
@@ -17,7 +19,6 @@ function parse_results(in, settings)
 	hist_a
 	round(hist_theta / 1e4)
 	hist_err'
-	error("ciao")
 
 	switch output
 		case ALL_HISTORY
