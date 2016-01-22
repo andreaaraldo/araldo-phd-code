1ff7a2a03c85bf19ff5c21487735f322c3e40cd1
andrea from cranberry <andrea.araldo@gmail.com>
2016-01-22 17:07:26 +0100

    AA

--- femtoCDN/parse_results.m
+++ femtoCDN/parse_results.m
@@ -29,7 +29,6 @@ function parse_results(in, settings)
 	theta_dopo_ancora = hist_theta(:, primo_negativo+1 )'
 	%}
 	configurazione = round(hist_theta(:, size(hist_theta, 2) ) )
-	error("ciao");
 	%errore = hist_err'
 	%}
 	switch output
