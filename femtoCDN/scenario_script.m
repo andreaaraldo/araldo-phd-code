86f1558f1d1b9d384783086fa1d5cb7a0709e537
Andrea Araldo <andrea.araldo@gmail.com>
2016-01-22 12:21:21 +0100

    AA

--- femtoCDN/scenario_script.m
+++ femtoCDN/scenario_script.m
@@ -1,7 +1,7 @@
 %script
 global severe_debug = 1;
 addpath("~/software/araldo-phd-code/utility_based_caching/scenario_generation");
-mdat_folder = "~/archive/femtoCDN/convergence_check_small_scale";
+mdat_folder = "~/remote_archive/femtoCDN/convergence_check_small_scale";
 max_parallel = 22;
 
 parse=false; % false if you want to run the experiment.
