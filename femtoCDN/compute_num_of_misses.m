69a730d7668b5b40a2fdc15c43d86ad05b30a6cc
Andrea Araldo <andrea.araldo@gmail.com>
2016-01-22 17:36:16 +0100

    AA

--- femtoCDN/compute_num_of_misses.m
+++ femtoCDN/compute_num_of_misses.m
@@ -6,9 +6,6 @@ function [num_of_misses, tot_requests, F] = compute_num_of_misses(in, theta, lam
 
 		requests = poissrnd(lambdatau); % one row per each CP, one cell per each object
 
-		requests_is = sum(requests, 2)'
-
-
 		max_catalog = max(in.catalog);
 		ordinal = repmat(1:max_catalog, p, 1);
 		cache_indicator_negated = ordinal > repmat(theta,1,max_catalog);
@@ -17,9 +14,12 @@ function [num_of_misses, tot_requests, F] = compute_num_of_misses(in, theta, lam
 			num_of_misses = [num_of_misses; requests(j,:) * cache_indicator_negated(j,:)'];
 		end
 
-		num_of_misses_is = num_of_misses'
-
 		tot_requests = sum(sum(requests,2)); % total requests, one cell per each CP
+
+		requests_is = sum(requests, 2)'
+		num_of_misses_is = num_of_misses'
+		tot_requests_is = tot_requests'
+
 		F = zeros(in.p, 1);
 		idx_selector = tot_requests != 0;
 		F(tot_requests != 0) = sum(requests,2)(idx_selector) / tot_requests((idx_selector));
