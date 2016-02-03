c8246607c6100f54bcbf9a49947d89bd60da048e
Andrea Araldo <andrea.araldo@gmail.com>
2016-02-03 21:53:48 +0100

    AA

--- femtoCDN/scenario_script.m
+++ femtoCDN/scenario_script.m
@@ -7,7 +7,7 @@ max_parallel = 24;
 
 
 
-parse=true; % false if you want to run the experiment.
+parse=false; % false if you want to run the experiment.
 settings.save_mdat_file = true;
 overwrite = true;
 methods_ = {"csda", "dspsa_orig", "opencache", "optimum"};
