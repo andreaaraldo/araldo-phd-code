1b2591d088ff66e5426eebb8b6d679251f755421
Andrea Araldo <andrea.araldo@gmail.com>
2016-02-04 10:28:11 +0100

    AA

--- femtoCDN/parse_results.m
+++ femtoCDN/parse_results.m
@@ -29,13 +29,11 @@ function parse_results(in, settings)
 			printf("%s %g %g %g\n", settings.method, in.lambda, in.T, v);
 
 		case FINAL_ERR
-			how_many = length(hist_err);
-			%how_many = 1800 / in.T;
+			%how_many = length(hist_err);
+			how_many = 1800 / in.T;
 			v1 = hist_err( how_many );
-			v1 = round(v1*100);
 			v2 = mean(hist_err(1:how_many) );
-			v2 = round(v2*100);
-			printf("%s %d %s %g %g %g %g %g\n", settings.method, settings.coefficients, settings.balancer_str, in.lambda, in.K, in.T, v1, v2);
+			printf("%d %g %g %g %g %g\n", settings.coefficients, in.lambda, in.K, in.T, v1, v2);
 
 		case FINAL_OBSERVED_HIT
 			v = hist_cum_hit(1);
