eab87bd5e37c54b8490b68fddee788bd014163d0
Andrea Araldo <andrea.araldo@gmail.com>
2016-02-05 15:03:32 +0100

    AA

--- femtoCDN/parse_results.m
+++ femtoCDN/parse_results.m
@@ -1,32 +1,27 @@
 function parse_results(in, settings)
 	HIST_REL_ERR=1; FINAL_CV=2; FINAL_OBSERVED_HIT=3; FINAL_ERR = 4; ERR_HISTORY = 5; HIST_STEPS=6; HIST_TRANSMISSIONS=7;
-	HIST_AVG_ERR=8;
-	output = HIST_REL_ERR;
+	HIST_AVG_ERR=8; HIST_NICE_ERR=9;
+	output = HIST_NICE_ERR;
 
 	%printf("\n\n\n\n I AM PRINTING %s %d\n", settings.method, settings.coefficients);
 
 	load(settings.outfile);
 	theta_opt = in.req_proportion' * in.K;
+	theta_unif = repmat(in.K/in.p, in.p, 1);
 
 
 	[hist_allocation, hist_cum_tot_requests, hist_cum_hit] = compute_metrics(...
 		in, settings, hist_theta, hist_num_of_misses, hist_tot_requests);
 
-	hist_theta = repmat(in.K/4, 1, size(hist_theta,2) );
 
 	hist_difference = ( hist_theta - repmat(theta_opt,1, size(hist_theta,2)) );
 	hist_difference_sqr = hist_difference .^ 2;
 	hist_difference_norm = sqrt( sum(hist_difference_sqr, 1) );
 	hist_CV = sqrt( meansq( hist_difference , 1 ) ) ./ mean(hist_theta, 1) ;
+	hist_nice_err = sqrt( meansq( hist_difference , 1 ) ) ./ in.K ;
 	hist_rel_err = hist_difference_norm ./  repmat( norm(theta_opt), 1, size(hist_difference,2) ) ;
 	hist_avg_err = (1/(in.K*in.p) ) * sum(abs( hist_difference ),1);
 	hist_weigth_avg_err = (1/p) * (1./theta_opt)' * abs(hist_difference);
-
-	%hist_difference_norm
-	%denominatore = repmat( norm(theta_opt), 1, size(hist_difference,2) )
-	theta_opt
-	norm(theta_opt)
-	error "cancella"
 
 	switch output
 
@@ -60,6 +55,11 @@ function parse_results(in, settings)
 			dlmwrite(result_file,  hist_avg_err' , " " );
 			printf("%s written\n", result_file);
 
+		case HIST_NICE_ERR
+			result_file = sprintf("%s.nice_err.dat", settings.simname);
+			dlmwrite(result_file,  hist_nice_err' , " " );
+			printf("%s written\n", result_file);
+
 		case HIST_TRANSMISSIONS
 			result_file = sprintf("%s.transmissions.dat", settings.simname);
 			num_of_transmissions = sum(hist_num_of_misses,1) .+ hist_updates;
@@ -71,6 +71,9 @@ function parse_results(in, settings)
 			end
 			dlmwrite( result_file, cum_num_of_transmissions', "");
 			printf("%s written\n", result_file);
+
+		otherwise
+			error "metric not recognized"
 	end
 
 end
