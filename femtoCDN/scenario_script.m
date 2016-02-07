88219561f34d3ca637a7836a3a70ec2c501e4f65
andrea from cranberry <andrea.araldo@gmail.com>
2016-02-08 00:18:18 +0100

    AA

--- femtoCDN/scenario_script.m
+++ femtoCDN/scenario_script.m
@@ -2,7 +2,7 @@
 global severe_debug = 1;
 addpath("~/software/araldo-phd-code/utility_based_caching/scenario_generation");
 mdat_folder = "~/remote_archive/femtoCDN/with10CPs";
-max_parallel = 4;
+max_parallel = 1;
 
 
 parse=false; % false if you want to run the experiment.
