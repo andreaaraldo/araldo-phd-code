2ba4299c700009f0cd7d5918b0b08099f03e6d39
Andrea Araldo <andrea.araldo@gmail.com>
2016-02-09 13:06:29 +0100

    AA

--- femtoCDN/parse_results.m
+++ femtoCDN/parse_results.m
@@ -1,7 +1,7 @@
 function parse_results(in, settings)
 	HIST_REL_ERR=1; FINAL_CV=2; FINAL_OBSERVED_HIT=3; FINAL_ERR = 4; ERR_HISTORY = 5; HIST_STEPS=6; 
 	HIST_MISSES=7; HIST_AVG_ERR=8; HIST_NICE_ERR=9; HIST_INFTY_ERR=10; HIST_GHAT_AVG=11;
-	output = HIST_STEPS;
+	output = HIST_MISSES;
 
 	%printf("\n\n\n\n I AM PRINTING %s %d\n", settings.method, settings.coefficients);
 
