55c930b0c3579e8ad2d005a200192cbbba84f720
andrea from cranberry <andrea.araldo@gmail.com>
2016-02-12 19:56:09 +0100

    AA

--- femtoCDN/scenario_script2.m
+++ femtoCDN/scenario_script2.m
@@ -5,9 +5,9 @@ mdat_folder = "~/remote_archive/femtoCDN/convergence_check_small_scale";
 max_parallel = 12;
 
 
-parse=false; % false if you want to run the experiment.
+parse=true; % false if you want to run the experiment.
 settings.save_mdat_file = true;
-overwrite = true;
+overwrite = false;
 
 methods_ = {"csda", "dspsa_orig", "opencache", "optimum", "unif", "optimum_nominal"};
 methods_ = {"opencache", "unif"};
@@ -16,8 +16,7 @@ methods_ = {"opencache", "unif"};
 normalizes = {"no", "max", "norm"};
 normalizes = {"no"};
 coefficientss = {"no", "simple", "every10","every100", "adaptive","adaptiveaggr", "insensitive", "smoothtriang", "triang"};
-coefficientss = {"adaptive","adaptiveaggr", "insensitive", "smoothtriang", "triang"};
-coefficientss = {"smart"}
+coefficientss = {"adaptive","adaptiveaggr", "insensitive", "smoothtriang", "triang","smart"};
 boosts = [1];
 lambdas = [1 10 100]; %req/s
 tot_times = [1]; %total time(hours)
