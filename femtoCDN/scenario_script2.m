5e1149c20cd6054846472e21f567ccbe11e9a3ad
Andrea Araldo <andrea.araldo@gmail.com>
2016-02-15 13:19:49 +0100

    AA

--- femtoCDN/scenario_script2.m
+++ femtoCDN/scenario_script2.m
@@ -5,7 +5,7 @@ mdat_folder = "~/remote_archive/femtoCDN/convergence_check_small_scale";
 max_parallel = 12;
 
 
-parse=true; % false if you want to run the experiment.
+parse=false; % false if you want to run the experiment.
 clean_tokens=false;
 settings.save_mdat_file = true;
 overwrite = false;
