448e276f9c9f388198cc039cac43f20badb8ee6e
andrea from cranberry <andrea.araldo@gmail.com>
2016-02-16 19:06:25 +0100

    AA

--- femtoCDN/scenario_script.m
+++ femtoCDN/scenario_script.m
@@ -5,7 +5,7 @@ mdat_folder = "~/remote_archive/femtoCDN/new";
 max_parallel = 8;
 
 
-parse=false; % false if you want to run the experiment.
+parse=true; % false if you want to run the experiment.
 clean_tokens=false;
 settings.save_mdat_file = true;
 overwrite = false;
