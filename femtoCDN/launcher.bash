ac9379970080cc93e082cfff25620fa0775f7422
Andrea Araldo <andrea.araldo@gmail.com>
2016-02-18 10:57:25 +0100

    AA

--- femtoCDN/launcher.bash
+++ femtoCDN/launcher.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-screen /bin/bash -c 'octave --quiet scenario_script.m > /tmp/femtoCDN.log'
+screen /bin/bash -c 'octave --quiet misses_after_60_min-eyeball-large_scale.scenario.m > /tmp/femtoCDN2.log'
