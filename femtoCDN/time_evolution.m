885d54f91efed63b0561eb659711c1bdc5ce31f4
araldo_tipi53 <andrea.araldo@gmail.com>
2016-02-24 18:36:31 +0100

    AA

--- femtoCDN/time_evolution.m
+++ femtoCDN/time_evolution.m
@@ -28,7 +28,7 @@ coefficientss = {"linearhalved5", "halved5re30"};
 boosts = [1];
 lambdas = [100]; %req/s 
 tot_times = [2]; %total time(hours)
-Ts = [10]; % epoch duration (s)
+Ts = [100]; % epoch duration (s)
 overall_ctlgs = [3.5e6];
 overall_ctlgs = [1e4];
 
