65a00544f2e1b8dedb681717db2ba1c3fed3d1d5
andrea from cranberry <andrea.araldo@gmail.com>
2016-03-02 10:12:12 +0100

    AA

--- femtoCDN/improvement.m
+++ femtoCDN/improvement.m
@@ -11,7 +11,7 @@ warning("error", "Octave:divide-by-zero");
 parse=false; % false if you want to run the experiment.
 clean_tokens=false;
 settings.save_mdat_file = true;
-overwrite = false;
+overwrite = true;
 compact_name=true;
 
 methods_ = {"csda", "dspsa_orig", "opencache", "optimum", "unif", "optimum_nominal","declaration"};
