a95fef63edd3edb855536683d8b4a9fb6d6b7c3a
Andrea Araldo <andrea.araldo@gmail.com>
2016-03-06 18:08:50 +0100

    AA

--- femtoCDN/time_evolution.m
+++ femtoCDN/time_evolution.m
@@ -3,7 +3,7 @@ global severe_debug = 1;
 addpath("~/software/araldo-phd-code/utility_based_caching/scenario_generation");
 addpath("~/software/araldo-phd-code/general/statistical/");
 settings.mdat_folder = "~/remote_archive/femtoCDN/new/time_evolution";
-max_parallel = 23;
+max_parallel = 7;
 warning("error", "Octave:divide-by-zero");
 
 
