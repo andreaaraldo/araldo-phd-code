3fb56226abdfbd48bf3a420f866b73abe01e4371
andrea from cranberry <andrea.araldo@gmail.com>
2016-03-09 16:17:43 +0100

    AA

--- femtoCDN/parse_results.m
+++ femtoCDN/parse_results.m
@@ -142,7 +142,7 @@ function parse_results(in, settings)
 			pointmisses = sum(hist_num_of_misses,1) ./ hist_tot_requests;
 			vs = zeros(length(pointmisses));
 			for t=1:length(pointmisses)
-				vs(t) = prctile(pointmisses(1:t) ,5);
+				vs(t) = prctile(pointmisses(1:t)' ,5);
 			end
 			result_file = sprintf("%s.prctile.dat", settings.simname);
 			dlmwrite( result_file, vs, "");
