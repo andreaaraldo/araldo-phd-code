c5b7d8282d10756fc928356b2529770b44186544
Andrea Araldo <andrea.araldo@gmail.com>
2016-03-09 15:28:58 +0100

    AA

--- femtoCDN/compute_simname.m
+++ femtoCDN/compute_simname.m
@@ -3,7 +3,7 @@ function simname = compute_simname(settings, in)
 
 global COEFF_LINEARHALVED5; global COEFF_NO;
 global COEFF_LINEARHALVED5REINIT30MIN; global COEFF_LINEARHALVED5REINIT1DAY;
-global COEFF_TRIANGULAR;
+global COEFF_TRIANGULAR; global COEFF_MODERATE;
 
 global NORM_NO;
 global PROJECTION_NO; global PROJECTION_EUCLIDEAN;
@@ -63,6 +63,8 @@ global PROJECTION_NO; global PROJECTION_EUCLIDEAN;
 												in.coefficients_str="halved5re1d";
 											case COEFF_TRIANGULAR
 												in.coefficients_str="triang";
+											case COEFF_MODERATE
+												in.coefficients_str="moderate";
 											otherwise
 												settings.coefficients
 												error "Error: coefficient not recognized"
