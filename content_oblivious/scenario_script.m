fb80bd605abcdb48c454e24972fac074dd1f10e7
Andrea Araldo <andrea.araldo@gmail.com>
2016-10-19 11:23:19 +0200

    AA: evaluating the downloads to the cache

--- content_oblivious/scenario_script.m
+++ content_oblivious/scenario_script.m
@@ -8,7 +8,7 @@ warning ("error", "Octave:broadcast");
 
 
 
-parse=true; % false if you want to run the experiment.
+parse=false; % false if you want to run the experiment.
 clean_tokens=false;
 settings.save_mdat_file = true;
 overwrite = true;
@@ -18,6 +18,7 @@ settings.ON_hist_trash=true;
 
 methods_ = {"csda", "dspsa_orig", "opencache", "optimum", "unif", "optimum_nominal","declaration"};
 methods_ = {"opencache","unif","optimum"};
+methods_ = {"opencache"};
 
 
 normalizes = {"no", "max", "norm"};
@@ -25,10 +26,12 @@ normalizes = {"no"};
 coefficientss = {"no", "simple", "every10","every100", "adaptive","adaptiveaggr", "insensitive", "smoothtriang", "triang"};
 coefficientss = {"adaptive","adaptiveaggr", "insensitive", "smoothtriang", "triang", "smartsmooth", "linear", "moderate", "moderatelong", "linearlong","linearsmart10", "linearsmart100"};
 coefficientss = {"triang", "moderate", "linearhalved5"};
+coefficientss = {"linearhalved5"};
+
 boosts = [1];
 lambdas = [100]; %req
-tot_times = [1,5, 10, 50, 100]; %total time(hours)
-Ts = [10]; % epoch duration (s)
+tot_times = [1]; %total time(hours)
+Ts = [1,5, 10, 50, 100]; % epoch duration (s)
 overall_ctlgs = [1e8];
 CTLG_PROP=-1; % To split the catalog as the request proportion
 ctlg_epss = [0];
@@ -45,7 +48,7 @@ Ks = [1e6]; %cache slots
 projections = {"no", "fixed", "prop", "euclidean"};
 projections = {"euclidean"};
 knows=[Inf]; %knowledge degree value
-seeds = 1:20;
+seeds = 1;
 
 
 
