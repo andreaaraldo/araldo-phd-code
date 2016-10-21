8e9ec3d3482c1a7c39ae3eb4f435c4152918cea7
Andrea Araldo <andrea.araldo@gmail.com>
2016-10-21 16:40:02 +0200

    AA

--- content_oblivious/scenario_script.m
+++ content_oblivious/scenario_script.m
@@ -8,7 +8,7 @@ warning ("error", "Octave:broadcast");
 
 
 
-parse=true; % false if you want to run the experiment.
+parse=false; % false if you want to run the experiment.
 clean_tokens=false;
 settings.save_mdat_file = true;
 overwrite = false;
@@ -18,6 +18,7 @@ settings.ON_hist_trash=true;
 
 methods_ = {"csda", "dspsa_orig", "opencache", "optimum", "unif", "optimum_nominal","declaration"};
 methods_ = {"opencache","unif","optimum"};
+methods_ = {"optimum"};
 
 
 normalizes = {"no", "max", "norm"};
@@ -30,7 +31,7 @@ boosts = [1];
 lambdas = [100]; %req
 tot_times = [1]; %total time(hours)
 Ts = [1,5, 10, 50, 100]; % epoch duration (s)
-Ts = [100]; % epoch duration (s)
+Ts = [1000]; % epoch duration (s)
 overall_ctlgs = [1e8];
 CTLG_PROP=-1; % To split the catalog as the request proportion
 ctlg_epss = [0];
