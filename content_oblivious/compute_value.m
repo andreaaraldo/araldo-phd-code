697bb2b8ef7e54689df7434bc0728ff19e83691a
Andrea Araldo <andrea.araldo@gmail.com>
2016-10-21 16:30:02 +0200

    AA: evaluating the downloads to the cache

--- content_oblivious/compute_value.m
+++ content_oblivious/compute_value.m
@@ -1,7 +1,14 @@
 function v = compute_value(in, theta)
 	v = 0;
 	for j=1:in.p
-		[cdf_value, h] = ZipfCDF_smart(floor(theta(j) ), 0, [], in.alpha(j), [], in.ctlg(j), in.estimated_rank(j,:) );
+			estimated_rank = [];
+			if in.know < Inf
+				% If each CP knows exactly the popularity of its catalog,
+				% we do not need the estimated_rank data structure for our computation
+				estimated_rank = in.estimated_rank(j,:)';
+			end
+
+		[cdf_value, h] = ZipfCDF_smart(floor(theta(j) ), 0, [], in.alpha(j), [], in.ctlg(j), estimated_rank );
 		v += cdf_value * in.req_proportion(j);
 	end
 end
