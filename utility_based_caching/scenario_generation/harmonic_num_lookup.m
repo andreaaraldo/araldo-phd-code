30b3c4f3d9bf83081bd0fa77fc888a1061bc8714
Andrea Araldo <andrea.araldo@gmail.com>
2016-10-21 13:31:53 +0200

    AA: evaluating the downloads to the cache

--- utility_based_caching/scenario_generation/harmonic_num_lookup.m
+++ utility_based_caching/scenario_generation/harmonic_num_lookup.m
@@ -1,6 +1,6 @@
 % Return known harmonic numbers
 function harmonic_num = harmonic_num_lookup(ctlg_size, alpha)
-	table = [1e8, 0.8, -1];
+	table = [1e8, 0.8, 0.0082538];
 	score=sum( repmat([ctlg_size, alpha], size(table,1),1) == table(:,1:2), 2);
 	entry = find(score==2);
 	harmonic_num = table(entry,3);
