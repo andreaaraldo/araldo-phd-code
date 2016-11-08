93324d2388a8a2e914a2afe96dfc5c10b9c69178
Andrea Araldo <andrea.araldo@gmail.com>
2016-11-08 12:04:27 +0100

    AA: adding confidence intervals to journal versione

--- ccnsim-cost/scripts/runall.sh
+++ ccnsim-cost/scripts/runall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 main=./ccnSim
-NUM_OF_PROCESSORS=8;
+NUM_OF_PROCESSORS=4;
 
 runs=`$main -x General $1 | awk '/runs/{print $4}'`
 cp ./omnetpp.ini /tmp/ccnsim_output-`date "+%Y-%m-%d_%Hh%M.%S"`.ini
