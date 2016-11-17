c1e412140efec213f3610aaffcb47026e5a53ef5
Andrea Araldo <andrea.araldo@gmail.com>
2016-11-17 11:51:27 +0100

    AA: adding confidence intervals to journal versione

--- general/process_results/mean_and_conf.m
+++ general/process_results/mean_and_conf.m
@@ -11,6 +11,8 @@ function mean_and_conf(input_files)
 	sizes = size(dlmread(input_files{1} ) );
 	table = zeros(sizes(1), sizes(2), length(input_files) );
 
+	printf("Processing %d files\n", length(input_files) );
+
 	for i=1:length(input_files )
 		file = input_files{i};
 		table( :,:, i ) = dlmread(file);
