6fa4b49deb45d8d9b951cb889bf3adc871d93def
Andrea Araldo <andrea.araldo@gmail.com>
2016-11-18 15:28:34 +0100

    AA

--- utility_based_caching/examples/multi_as/scenario_script.m
+++ utility_based_caching/examples/multi_as/scenario_script.m
@@ -33,16 +33,16 @@ fixed_data.utilities = [0, 1/5, 2/5, 3/5, 4/5, 5/5 ];
 fixed_data.name = "linear";
 data.fixed_datas = [data.fixed_datas, fixed_data];
 
-data.seeds = [1];
+data.seeds = [2];
 
 data.cache_allocations = {"constrained"}; # constrained or free
 data.solutiongaps = [0]; # default 0.0001 (that means 0.01%)
 data.timelimits = [57700]; # default 1e75
-data.catalog_sizes = [10];
+data.catalog_sizes = [100];
 data.cache_to_ctlg_ratios = [1*10/100];	% fraction of catalog we could store in the overall cache space
 											% if all the objects were at maximum quality
 data.alphas = [1];
-data.customtypes = {"int"}; % float or int	
+data.customtypes = {"int","float"}; % float or int	
 %{ TOPOLOGY
 	data.topofile="";
 	if ( strcmp(data.topofile,"") )
--- utility_based_caching/scenario_generation/generate_mod_file.m
+++ utility_based_caching/scenario_generation/generate_mod_file.m
@@ -14,14 +14,8 @@ mod_filename = sprintf("%s/model.mod",singledata.fixed_data.path_base);
 
 	command = sprintf("sed -i.bak 's_/\\*%s_ _' %s; sed -i.bak 's_%s\\*/_ _' %s; sed -i.bak 's_CUSTOMTYPE_%s_' %s; sed -i.bak 's_CACHETYPE_%s_' %s ",...
 			singledata.strategy,singledata.mod_filename, singledata.strategy,singledata.mod_filename,...
-<<<<<<< HEAD
 			singledata.customtype, singledata.mod_filename,...
 			cachetype, singledata.mod_filename);
-	command
-	error "ciao"
-=======
-			singledata.customtype, singledata.mod_filename );
->>>>>>> cd41c88c1bd484faeb053933e8c841cfd5d0fb56
 	[status, output] = system(command,1);
 
 	if (status != 0)
