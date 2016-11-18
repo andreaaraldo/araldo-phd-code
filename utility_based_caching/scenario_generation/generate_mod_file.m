7edb30671f78187d1111d5f0485e4bb7f7c20979
Andrea Araldo <andrea.araldo@gmail.com>
2016-11-18 13:20:11 +0100

    AA: video caching journal

--- utility_based_caching/scenario_generation/generate_mod_file.m
+++ utility_based_caching/scenario_generation/generate_mod_file.m
@@ -4,9 +4,18 @@ mod_filename = sprintf("%s/model.mod",singledata.fixed_data.path_base);
 	copyfile("~/software/araldo-phd-code/utility_based_caching/model.mod",...
 			 singledata.mod_filename );
 
-	command = sprintf("sed -i.bak 's_/\\*%s_ _' %s; sed -i.bak 's_%s\\*/_ _' %s; sed -i.bak 's_CUSTOMTYPE_%s_' %s ",...
+	cachetype="";
+	switch singledata.customtype
+		case "int"
+			cachetype="boolean";
+		case "float"
+			cachetype="float";
+	end
+
+	command = sprintf("sed -i.bak 's_/\\*%s_ _' %s; sed -i.bak 's_%s\\*/_ _' %s; sed -i.bak 's_CUSTOMTYPE_%s_' %s; sed -i.bak 's_CACHETYPE_%s_' %s ",...
 			singledata.strategy,singledata.mod_filename, singledata.strategy,singledata.mod_filename,...
-			singledata.customtype, singledata.mod_filename );
+			singledata.customtype, singledata.mod_filename,...
+			cachetype, singledata.mod_filename);
 	command
 	error "ciao"
 	[status, output] = system(command,1);
