2b79052b916c4d097a711e0685081892b8229c38
Andrea Araldo <andrea.araldo@gmail.com>
2016-11-18 10:59:40 +0100

    AA: video caching journal

--- utility_based_caching/scenario_generation/generate_mod_file.m
+++ utility_based_caching/scenario_generation/generate_mod_file.m
@@ -7,6 +7,8 @@ mod_filename = sprintf("%s/model.mod",singledata.fixed_data.path_base);
 	command = sprintf("sed -i.bak 's_/\\*%s_ _' %s; sed -i.bak 's_%s\\*/_ _' %s; sed -i.bak 's_CUSTOMTYPE_%s_' %s ",...
 			singledata.strategy,singledata.mod_filename, singledata.strategy,singledata.mod_filename,...
 			singledata.customtype, singledata.mod_filename );
+	command
+	error "ciao"
 	[status, output] = system(command,1);
 
 	if (status != 0)
