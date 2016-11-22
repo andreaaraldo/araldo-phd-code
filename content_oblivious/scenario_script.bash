8bfdac1d376b515127c2a5559400a507250956ab
Andrea Araldo <andrea.araldo@gmail.com>
2016-11-22 17:43:47 +0100

    AA

--- content_oblivious/scenario_script.bash
+++ content_oblivious/scenario_script.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-screen /bin/bash -c 'octave --quiet --no-gui-libs scenario_script.m 2>&1 ~/scenario.log'
+screen /bin/bash -c 'octave --quiet --no-gui-libs scenario_script.m > ~/scenario.log'
