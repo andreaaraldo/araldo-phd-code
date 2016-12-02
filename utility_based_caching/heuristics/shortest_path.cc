a88392de97fa88062b479070364596261f2ef54f
Andrea Araldo <andrea.araldo@gmail.com>
2016-12-02 18:51:55 +0100

    AA: video caching journal

--- utility_based_caching/heuristics/shortest_path.cc
+++ utility_based_caching/heuristics/shortest_path.cc
@@ -6,49 +6,100 @@
 #include <algorithm>                        // for std::for_each
 #include <boost/utility.hpp>                // for boost::tie
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <boost/property_map/property_map.hpp>
 #include <boost/graph/graphviz.hpp>
+#include "shortest_path.h"
 
 
 using namespace boost;
 using namespace std;
 
-int main(int,char*[])
-{
-	// create a typedef for the Graph type
-	typedef adjacency_list<vecS, vecS, undirectedS,
-		no_property, property<edge_weight_t, float> > Graph;
-	typedef graph_traits<Graph>::vertex_descriptor Vertex;
-
-	// writing out the edges in the graph
-	typedef std::pair<int, int> E;
-	E edges[] = {E(1,2), E(1,11), E(2,11), E(10,11), E(2,3),
+const float single_node_capacity = 10;
+Vertex ASes_with_users_[] = {5};
+E edges_[] = {E(1,2), E(1,0), E(2,0), E(10,0), E(2,3),
 		E(3,4), E(4,5), E(5,6), E(6,7), E(4,8), E(7,8), E(3,9), 
 		E(8,9), E(9,10)};
+Weight weights[] = {1, 1, 1, 1, 1,
+						1, 1, 1, 1, 1.01, 1, 1,
+						1, 1};
 
-	const int num_edges = sizeof(edges)/sizeof(edges[0]);
-	const int num_nodes = 11;
-	float lambdas[num_edges]; // Lagrange multipliers
-	int weights[num_edges];
-	int weights_temp[num_edges];
-	for (int j=0; j<num_edges; j++) {weights[j]=0; lambdas[j]=1;}
 
-	float obj_size = 1;
-	for (int j=0; j<num_edges; j++) {weights_temp[j] = weights[j]+obj_size*lambdas[j];}
+const int server_idx = 8;
+
+unsigned count_nodes(vector<E> edges)
+{
+	//Count the number of nodes
+	// The max number of nodes is the num of edges +1
+	set<int> nodes;
+	for (int j=0; j < edges.size(); j++)
+	{
+		nodes.insert(edges[j].first); nodes.insert(edges[j].second);
+	}
+	int j=0;
+	for (set<int>::iterator it=nodes.begin(); it!=nodes.end(); ++it, j++)
+    	if(j!=*it){
+			cout<<"Nodes should be numbered from 0 to "<<nodes.size()-1<<endl; exit(0);
+		}
+	return nodes.size();
+}
+
+
+// Returns the distance between each client and the source
+ClientSourceDistanceMap compute_distances_from_source(
+	Vertex source, vector<Vertex> ASes_with_users, Graph G)
+{
+
+	IndexMap indexMap = boost::get(boost::vertex_index, G);
+	boost::property_map<Graph, boost::edge_weight_t>::type EdgeWeightMap = 
+				get(boost::edge_weight_t(), G);
+
+		// vector for storing distance property
+		vector<Weight> distances(num_vertices(G));
+		vector<Vertex> predecessors(num_vertices(G));
+		PredecessorMap predecessorMap(&predecessors[0], indexMap);
+		DistanceMap distanceMap(&distances[0], indexMap);
+		dijkstra_shortest_paths(G, source, 
+			distance_map(distanceMap).predecessor_map(predecessorMap) );
+
+		ClientSourceDistanceMap client_distances;
+		Weight cost=0;
+		for (int j=0; j<ASes_with_users.size(); j++)
+		{
+			Vertex client = vertex(ASes_with_users[j],G);
+			client_distances.emplace(std::make_pair(client,source), distances[client]);
+
+			PathType path;
+			Vertex v=client;
+			for (Vertex u = predecessorMap[v]; u!=v; v=u, u=predecessorMap[v])
+			{
+				std::pair<Graph::edge_descriptor, bool> edgePair = boost::edge(u, v, G);
+				Graph::edge_descriptor edge = edgePair.first;		 
+				path.push_back( edge );
+				cost += EdgeWeightMap[edge];
+			}
+		}
+	return client_distances;
+}
+
+
+int main(int,char*[])
+{
+
+	// writing out the edges in the graph
+	vector<E> edges(edges_, edges_+sizeof(edges_)/sizeof(E) );
+	vector<Vertex> ASes_with_users(ASes_with_users_, ASes_with_users_+
+		sizeof(ASes_with_users_)/sizeof(Vertex) );
+	
+	unsigned num_nodes = count_nodes(edges);
+	cout << "count_nodes = "<< num_nodes <<endl;
 
     // declare a graph object
-    Graph G(edges, edges + sizeof(edges) / sizeof(E), weights_temp, num_nodes);
+    Graph G(edges_, edges_ + sizeof(edges_)/sizeof(E), weights, num_nodes);
+
+	ClientSourceDistanceMap client_source_distances = 
+			compute_distances_from_source(vertex(server_idx,G), ASes_with_users, G);
 	
-	// vector for storing distance property
-	std::vector<int> distance(num_nodes);
-	Vertex source = vertex(1,G);
-	dijkstra_shortest_paths(G, source, distance_map(&distance[0]));
-
-	cout << "distances from start vertex:" << endl;
-	graph_traits<Graph>::vertex_iterator vi;
-	for(vi = vertices(G).first; vi != vertices(G).second; ++vi)
-		cout << "distance(" << index(*vi) << ") = " 
-              << distance[*vi] << endl;
-	cout << endl;
-
     return 0;
 }
---
+++ utility_based_caching/heuristics/shortest_path.h
@@ -0,0 +1,31 @@
+#include <boost/config.hpp>
+#include <iostream>                         // for std::cout
+#include <utility>                          // for std::pair
+#include <algorithm>                        // for std::for_each
+#include <boost/utility.hpp>                // for boost::tie
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <boost/graph/graphviz.hpp>
+
+
+#ifndef SHORTEST_PATH_H_
+#define SHORTEST_PATH_H_
+
+using namespace boost;
+
+//{ TYPES
+	// create a typedef for the Graph type
+	typedef float Weight;
+	typedef adjacency_list<vecS, vecS, undirectedS,
+		no_property, property<edge_weight_t, float> > Graph;
+	typedef graph_traits<Graph>::vertex_descriptor Vertex;
+	typedef boost::property_map < Graph, boost::vertex_index_t >::type IndexMap;
+	typedef boost::iterator_property_map < Vertex*, IndexMap, Vertex, Vertex& > PredecessorMap;
+	typedef boost::iterator_property_map < Weight*, IndexMap, Weight, Weight& > DistanceMap;
+	typedef std::vector<Graph::edge_descriptor> PathType;
+	typedef std::pair<int, int> E;
+	typedef std::map< std::pair<Vertex, Vertex>, Weight > ClientSourceDistanceMap;
+//} TYPES
+#endif
